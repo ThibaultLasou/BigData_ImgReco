bc9a111432c2a6394e9ab6a5f01a68f962e3676f
YKertous <yasmine.kertous@u-psud.fr>
2017-12-21 23:44:59 +0100

    ajout q6

---
+++ ImgReco_2/q6.py
@@ -0,0 +1,63 @@
+#!/usr/bin/python3
+
+import numpy as np
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from torch.autograd import Variable
+import torch.optim as optim
+torch.set_printoptions(profile="full")
+torch.manual_seed(0)
+
+
+class CNN(nn.Module):
+
+    def __init__(self):
+            super(CNN, self).__init__()
+            self.conv1 = nn.Conv2d(1, 6, 5)
+            self.conv2 = nn.Conv2d(20, 16, 5)
+            self.fc1 = nn.Linear(4*4*50, 500)
+            self.fc2 = nn.Linear(500, 10)
+            self.fc3 = nn.Linear(500, 10)
+
+
+    def forward(self, x):
+            x = x.view(-1, 1, 28, 28)
+            x = F.relu(F.max_pool2d(self.conv1(x), 2))
+            print(x)
+            x = F.relu(F.max_pool2d(self.conv2(x), 2))
+            print(x)
+            x = x.view(x.size(0), -1) # Flatten the tensor
+            x = F.relu(self.fc1(x))
+            print(x)
+            x = x.view(x.size(0), -1) 
+            x = F.relu(self.fc2(x))
+            print(x)
+            x = F.log_softmax(self.fc3(x))
+            print(x)
+            
+            return x
+
+
+if __name__ == '__main__':
+    # Hyperparameters
+    epoch_nbr = 100
+    batch_size = 100
+    learning_rate = 1e-1
+
+    # Data loading
+    X0 = Variable(torch.from_numpy(np.load("data/trn_img.npy"))[:100].type(torch.FloatTensor))
+    lbl0 = Variable(torch.from_numpy(np.load("data/trn_lbl.npy"))[:100].type(torch.FloatTensor).long())
+    X1 = Variable(torch.from_numpy(np.load("data/dev_img.npy")).type(torch.FloatTensor))
+    lbl1 = Variable(torch.from_numpy(np.load("data/dev_lbl.npy")).type(torch.FloatTensor).long())
+
+    net = CNN()
+    optimizer = optim.SGD(net.parameters(), lr=learning_rate)
+    for e in range(epoch_nbr):
+        print("Epoch", e)
+        for i in range(0, X0.data.size(0), batch_size):
+            optimizer.zero_grad() # Reset all gradients to 0
+            predictions_train = net(X0[i:i+batch_size])
+            loss = F.nll_loss(predictions_train, lbl0[i:i+batch_size])
+            loss.backward()
+            optimizer.step() # Perform the weights update
